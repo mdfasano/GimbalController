78b7687b01ce5dec4ce281cee3510bb759e6a673
mdfasano <mdfasano16@gmail.com>
2026-03-10 09:42:16 -0400

    add rotation functions to controller. update main program to act as a canned demo of functionality

--- GimbalController/Program.cs
+++ GimbalController/Program.cs
@@ -11,6 +11,8 @@ System.Diagnostics.Debug.WriteLine("testing1111");
 
 System.Diagnostics.Debug.WriteLine("Starting Galil Test...");
 
+// this is written to be a canned demonstration of the movement/control of the gimbal machine
+// it is not representative of the final product
 
 GController gimbal = new();
 try
@@ -19,8 +21,16 @@ try
     gimbal.Connect("192.168.1.5 -direct");
 
     // Example: Tell Information
-    string info = gimbal.SendCommand("TI");
-    System.Diagnostics.Debug.WriteLine($"Controller says: {info}");
+    string info = gimbal.SendCommand("TI"); // prove we are connected
+    System.Diagnostics.Debug.WriteLine($"Controller says: {info}"); // need to do some string parsing here, I think response isnt legible
+
+    // rotate about axis A and back
+    gimbal.RotateRelative('A', 5000);
+    gimbal.RotateRelative('A', -5000);
+
+    // rotate about axis B and back
+    gimbal.RotateRelative('B', 5000);
+    gimbal.RotateRelative('B', -5000);
 }
 catch (Exception ex)
 {
